6b608ca1916203eb250c77b0eef7d66ac3285153
engineerErica <ericaponce2002@gmail.com>
2023-12-08 18:00:59 -0500

    error document not defined

--- public/index.js
+++ public/index.js
@@ -1,26 +1,27 @@
+const express = require('express');
+const app = express();
+app.get('/health', (req, res) => {
+    res.send('hello');
+})
+app.listen(3001, () => {
+    console.log('http://localhost:3001');
+});
+
 const pets = [];
 
 const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    const formObj = Object.fromEntries(formData);
-    formObj.isFriendly = formObj.isFriendly === 'on';
-    // console.log(formObj)
-    form.reset();
-    pets.push(formObj);
-    console.log(pets)
+  e.preventDefault();
+  const form = e.target;
+  const formData = new FormData(form);
+  const formObj = Object.fromEntries(formData);
+  formObj.isFriendly = formObj.isFriendly === 'on';
+  // console.log(formObj);
+  form.reset();
+  pets.push(formObj);
+  console.log(pets);
 }
 const main = () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', handleSubmit);
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
 }
 main();
-// const express = require('express');
-// const app = express();
-// app.get('/health', (req, res) => {
-//     res.send('hello');
-// })
-// app.listen(3001, () => {
-//     console.log('http://localhost:3001');
-// });
